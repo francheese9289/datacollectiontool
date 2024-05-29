391f51b9aa96bbe80a7c1174f496967d705f8b4c
unknown <elizabeth.francese@gmail.com>
2024-05-29 15:19:07 -0400

    gunicorn

--- run.py
+++ run.py
@@ -1,13 +1,25 @@
 from app import init_app
 import os
+from gunicorn.app.base import BaseApplication
 
+class FlaskApplication(BaseApplication):
+    def __init__(self, app):
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        pass
+    # self.cfg.set("workers", 2)
+    # self.cfg.set("timeout", 30)
+
+    def load(self):
+        return self.application
 
 app = init_app()
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    app.config['ENV'] = 'production'
-    # Run the Flask application
-    app.run()
+    # Set Flask environment to production
+    os.environ['FLASK_ENV'] = 'production'
 
+    # Run the Flask application using Gunicorn
+    FlaskApplication(app).run()
